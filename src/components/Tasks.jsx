1e5c85c5749e736682e1fe5c7503e6a150c37242
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-10-01 11:29:04 -0300

    New task function

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,13 +1,32 @@
 import { useContext } from 'react';
+import dateFormat from 'dateformat';
 import { TaskContext } from '../context/TaskProvider';
 import { UserContext } from '../context/UserProvider';
 
 const Tasks = () => {
-  const { getTasks } = useContext(TaskContext);
+  const {
+    TasksList, getTasks, sendTask, handleNewTaskChange,
+  } = useContext(TaskContext);
   const { Token } = useContext(UserContext);
-  if (Token) getTasks(Token);
+
+  if (!TasksList.length && Token) getTasks(Token);
+
   return (
-    <div />
+    <div>
+      <h2>Tasks</h2>
+      {TasksList
+        && TasksList.map(({ _id, text, created }) => (
+          <p key={_id}>
+            {`${text} ${dateFormat(created, 'hh:mm - dd/mm/yyyy')}`}
+          </p>
+        ))}
+      <section>
+        <h3>New task</h3>
+        form
+        <textarea name="taskText" id="taskText" cols="30" rows="10" onChange={handleNewTaskChange} />
+        <button type="button" onClick={() => sendTask(Token)}>Add</button>
+      </section>
+    </div>
   );
 };
 
--- src/context/TaskProvider.jsx
+++ src/context/TaskProvider.jsx
@@ -1,29 +1,58 @@
-/* eslint-disable no-unused-vars */
-import { useState, createContext, useContext } from 'react';
+import { useState, createContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { UserContext } from './UserProvider';
 
 const TaskContext = createContext();
 
 const TaskProvider = ({ children }) => {
   const [TasksList, setTasksList] = useState([]);
+  const [NewTask, setNewTask] = useState('');
+  useEffect(() => {}, [TasksList]);
 
   const getTasks = async (token) => {
     const URL = 'https://task-list-api-gmc.herokuapp.com/tasks';
-    const response = await fetch(URL, {
-      method: 'GET',
+    try {
+      const response = await fetch(URL, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: token,
+        },
+      }).then((res) => res.json());
+
+      if (response.tasks) {
+        setTasksList(response.tasks);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleNewTaskChange = ({ target }) => {
+    const { value } = target;
+
+    setNewTask(value);
+  };
+
+  const sendTask = async (token) => {
+    if (!NewTask) return;
+    const URL = 'https://task-list-api-gmc.herokuapp.com/tasks/create';
+    await fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify({ task: NewTask }),
       headers: {
         'Content-Type': 'application/json',
         Authorization: token,
       },
     }).then((res) => res.json());
-
-    if (response.tasks) {
-      setTasksList(response.tasks);
-    }
+    getTasks(token);
   };
 
-  const context = { TasksList, getTasks };
+  const context = {
+    TasksList,
+    getTasks,
+    sendTask,
+    handleNewTaskChange,
+  };
 
   return (
     <TaskContext.Provider value={context}>{children}</TaskContext.Provider>
